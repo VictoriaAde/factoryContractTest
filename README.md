40e2af6fe28ef60c28ebdeb9f240d01730cea436
Victoria <adedayovicky123@gmail.com>
2024-02-26 19:57:36 +0100

    SCRIPTS: interact with creatPoll function

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sample Hardhat Project
+# Voting Contract Interaction
 
 This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
 
@@ -11,3 +11,11 @@ REPORT_GAS=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.ts
 ```
+
+Transaction hash of contract creation
+0xc4ea7047cb34eef53f1538dbcf1e54aebd67adeca2f7039c6d2c7c53b8ac640e
+
+Transaction hash of interaction with createPoll function
+0x9767f9cf9a0f3b59482752db630f6e60097ee44adfa3cb59be827c25f7c9fd78
+
+[Link to contract on EtherScan](https://sepolia.etherscan.io/address/0x531A2F36969eF63016dd559e965fef9d1F3674F1)
--- scripts/interaction.ts
+++ scripts/interaction.ts
@@ -2,13 +2,22 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import { ethers } from "hardhat";
 
-describe("VotingPoll", () => {
-  async function deployVotingPollContract() {
-    const votingFactory = "0x531A2F36969eF63016dd559e965fef9d1F3674F1";
+async function main() {
+  const votingFactoryAddr = "0x531A2F36969eF63016dd559e965fef9d1F3674F1";
 
-    const tokenBank = await ethers.getContractAt(
-      "IVotingPollFactory",
-      votingFactory
-    );
-  }
+  const votingFactory = await ethers.getContractAt(
+    "IVotingPollFactory",
+    votingFactoryAddr
+  );
+
+  const question = "What is your president";
+  const options = ["Buhari", "Tinubu"];
+
+  const createPollTx = await votingFactory.createPoll(question, options);
+  await createPollTx.wait();
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
 });
